b379a6428d1b03f8a58a8ba82b2f647bc4080cb3
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2018-11-02 01:07:22 +0900

    feat: start ui

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export default () =>
+  <div>
+    Welcome to next.js!
+    <div>Very Good</div>
+  </div>
+
---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { parse } = require('url')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev, dir: 'src' })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+  server.get('*', (req,res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req,res,parsedUrl)
+  })
+  server.listen( port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
+
