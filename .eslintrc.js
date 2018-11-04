2393862f69edbf06de09c736c0dd92eaf6ac89b2
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2018-11-04 14:00:06 +0900

    dx: apply eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["standard"],
+  rules: {
+    "space-before-function-paren": 'off',
+    "keyword-spacing": 'off',
+    "no-multiple-empty-lines": 'warn',
+    "space-in-parens": 'off',
+    "no-multiple-empty-lines": ['warn', { "max": 2, "maxEOF": 1 }],
+    "comma-spacing": 'off',
+    "semi": ['error', 'never', { beforeStatementContinuationChars:'always'}],
+    "func-call-spacing": 'off',
+    "no-unexpected-multiline": 'off',
+    "no-unused-vars": 'warn',
+    "no-sequences": 'off',
+    "semi-style": ["error", "first"],
+    "space-unary-ops": ['error', { nonwords: true }],
+    "quotes": 'off',
+    "comma-dangle": ['warn', 'always-multiline'],
+    "object-curly-spacing": 'off',
+    "space-infix-ops": 'off',
+    "no-unexpected-multiline": 'error',
+  },
+};
+
