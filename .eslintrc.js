0b08645f822c9dfbc0ae3eb1d83224807f03f3ed
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2018-11-11 23:53:21 +0900

    dx: apply babel-eslint, eslint-plugin-react, @babel/plugin-proposal-optional-chaining

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
+  parser: 'babel-eslint',
   extends: ["standard"],
+  plugins: ['react'],
   rules: {
     "space-before-function-paren": 'off',
     "keyword-spacing": 'off',
@@ -13,12 +15,17 @@ module.exports = {
     "no-unused-vars": 'warn',
     "no-sequences": 'off',
     "semi-style": ["error", "first"],
-    "space-unary-ops": ['error', { nonwords: true }],
+    "space-unary-ops": 'off',
     "quotes": 'off',
     "comma-dangle": ['warn', 'always-multiline'],
     "object-curly-spacing": 'off',
     "space-infix-ops": 'off',
     "no-unexpected-multiline": 'error',
+    "padded-blocks": 'warn',
+
+    "standard/object-curly-even-spacing": 'off',
+    "react/jsx-uses-vars": ['warn'],
+    "react/jsx-no-undef": ['error'],
   },
   overrides: [
     {
